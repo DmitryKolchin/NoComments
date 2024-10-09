3856194b57eae26bf248bba469c40e2c055325ce
Dmitrii Kolchin <58472480+DmitryKolchin@users.noreply.github.com>
2024-10-09 21:29:39 +0100

    Importing and setting up fight animations

---
+++ Source/NoComments/DataAssets/CombatAnimationsDataAsset.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CombatAnimationsDataAsset.generated.h"
+
+/**
+ * Base class for data assets that store combat related animations
+ */
+UCLASS( Blueprintable )
+class NOCOMMENTS_API UCombatAnimationsDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY( EditDefaultsOnly, BlueprintReadOnly, Category = "Jabs" )
+	TArray<TSoftObjectPtr<UAnimMontage>> RightJabMontages;
+
+	UPROPERTY( EditDefaultsOnly, BlueprintReadOnly, Category = "Jabs" )
+	TArray<TSoftObjectPtr<UAnimMontage>> LeftJabMontages;
+};
