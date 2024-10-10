2b022515a86badd7b6c71449e53cf5810e0dd7bd
Dmitrii Kolchin <dmitry.k0l4in@gmail.com>
2024-10-10 21:36:06 +0100

    Hit damage was implemented

---
+++ Source/NoComments/Components/DamageDealingSphereComponent.cpp
@@ -0,0 +1,80 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DamageDealingSphereComponent.h"
+
+#include "Engine/DamageEvents.h"
+#include "GameFramework/Character.h"
+
+// Sets default values for this component's properties
+UDamageDealingSphereComponent::UDamageDealingSphereComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	OnComponentBeginOverlap.AddDynamic( this,  &UDamageDealingSphereComponent::OnBeginOverlap );
+	// ...
+}
+
+// Called every frame
+void UDamageDealingSphereComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
+// Called when the game starts
+void UDamageDealingSphereComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ACharacter* OwnerCharacter = Cast<ACharacter>( GetOwner() );
+	if (!IsValid( OwnerCharacter ))
+	{
+		ensureAlwaysMsgf( false, TEXT( "DamageDealingSphereComponent must be attached to a character" ) );
+		return;
+	}
+
+	USkeletalMeshComponent* OwnerMesh = OwnerCharacter->GetMesh();
+	if (!IsValid( OwnerMesh ))
+	{
+		ensureAlwaysMsgf( false, TEXT( "Owner character must have a skeletal mesh component" ) );
+		return;
+	}
+
+	UAnimInstance* OwnerAnimInstance = OwnerMesh->GetAnimInstance();
+
+	if (!IsValid( OwnerAnimInstance ))
+	{
+		ensureAlwaysMsgf( false, TEXT( "Owner character must have an anim instance" ) );
+		return;
+	}
+
+
+	// Binding self-destruction to the end of the montage
+	OwnerAnimInstance->OnMontageEnded.AddDynamic( this, &UDamageDealingSphereComponent::OnOwnerMontageEnded );
+
+	// ...
+}
+
+void UDamageDealingSphereComponent::OnOwnerMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	DestroyComponent(  );
+}
+
+void UDamageDealingSphereComponent::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor == GetOwner())
+	{
+		return;
+	}
+
+	if (!IsValid( OtherActor ))
+	{
+		return;
+	}
+
+	FDamageEvent DamageEvent;
+	OtherActor->TakeDamage( Damage, DamageEvent, GetOwner()->GetInstigatorController(), GetOwner() );
+}
---
+++ Source/NoComments/Components/DamageDealingSphereComponent.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "DamageDealingSphereComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable )
+class NOCOMMENTS_API UDamageDealingSphereComponent : public USphereComponent
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY( meta=(ExposeOnSpawn = true), BlueprintReadOnly  )
+	float Damage = 10.0f;
+
+public:
+	// Sets default values for this component's properties
+	UDamageDealingSphereComponent();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	UFUNCTION()
+	void OnOwnerMontageEnded( UAnimMontage* Montage, bool bInterrupted );
+
+	UFUNCTION()
+	void OnBeginOverlap( UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult );
+};
