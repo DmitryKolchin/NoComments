7083aa9299c65aa19ea9a8d8bb42809aadba92cc
Dmitrii Kolchin <dmitry.k0l4in@gmail.com>
2024-10-25 23:40:48 +0100

    Setting up the framework for combat design

---
+++ Source/NoComments/DataAssets/CombatSettingsDataAsset.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "CombatSettingsDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS( BlueprintType )
+class NOCOMMENTS_API UCombatSettingsDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY( EditDefaultsOnly, BlueprintReadWrite, Category = "Attack" )
+	float AttackSpeed = 2.f;
+
+	UPROPERTY( EditDefaultsOnly, BlueprintReadWrite, Category = "Block" )
+	float BlockWalkSpeed = 200.f;
+
+	UPROPERTY( EditDefaultsOnly, BlueprintReadWrite, Category = "Idle" )
+	float IdleWalkSpeed = 300.f;
+};
---
+++ Source/NoComments/DataAssets/CombatSettingsDataAsset_AI.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CombatSettingsDataAsset.h"
+#include "CombatSettingsDataAsset_AI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOCOMMENTS_API UCombatSettingsDataAsset_AI : public UCombatSettingsDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY( EditDefaultsOnly, BlueprintReadWrite, Category = "Attack" )
+	float AttackRange = 100.0f;
+
+	UPROPERTY( EditDefaultsOnly, BlueprintReadWrite, Category = "Idle" )
+	FFloatRange EnemyIdleTimeRange = FFloatRange( 1.f, 3.f );
+
+	UPROPERTY( EditDefaultsOnly, BlueprintReadWrite, Category = "Block" )
+	FFloatRange EnemyBlockTimeRange = FFloatRange( 0.5f, 1.5f );
+};
